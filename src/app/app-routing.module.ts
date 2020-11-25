96e35fd28cba998c3552ec4a4b9ab4fc8a7238af
anuragsr <anurag.131092@gmail.com>
2020-11-25 14:58:13 +0530

    Added fonts, scss vars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {DoctorProfileComponent} from './doctor-profile/doctor-profile.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DoctorProfileComponent } from './pages/doctor-profile/doctor-profile.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DoctorProfileComponent } from './pages/doctor-profile/doctor-profile.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { HeaderComponent } from './common/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DoctorProfileComponent
+    DoctorProfileComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
